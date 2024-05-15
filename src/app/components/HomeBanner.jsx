b86037acb050cfc0816e0f0075c86be5bde67cb9
BlarQ <collinsogunlala@gmail.com>
2024-05-15 16:20:28 +0100

    fix banner button

--- src/app/components/HomeBanner.jsx
+++ src/app/components/HomeBanner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import styles from "./homebanner.module.css";
+import styles from "./homebanner.module.css"
+import { MdArrowOutward } from "react-icons/md"
 
 export default function HomeBanner() {
   return (
@@ -8,7 +9,7 @@ export default function HomeBanner() {
         <div className='pl-16 pr-[40rem]'>
           <p className='text-7xl text-white font-extrabold'>Unlock the essence of timeless style<span className='text-[#2e4053]'>.</span></p>
 
-          <button className='bg-[#2e4053] mt-10 py-3 px-5 rounded-full text-white font-bold'>Shop Now</button>
+          <button className='flex bg-[#2e4053] mt-10 py-3 px-5 rounded-full text-white font-bold'>Shop Now <MdArrowOutward className='text-xl ml-1'/> </button>
         </div>
       </div>
     </div>
