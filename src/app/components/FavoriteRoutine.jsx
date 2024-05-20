010c239f13c53321aafc9614eb452b5e471497cc
BlarQ <collinsogunlala@gmail.com>
2024-05-20 23:06:13 +0100

    add: video files

--- src/app/components/FavoriteRoutine.jsx
+++ src/app/components/FavoriteRoutine.jsx
@@ -4,44 +4,69 @@ import React from 'react'
 import { MdArrowOutward } from 'react-icons/md'
 
 export default function FavoriteRoutine() {
-  return (
-    <div className='my-10 mx-8'>
-        <div className='flex justify-between items-end'>
-            <h2 className='text-6xl font-bold text-[#2e4053]'>Your Favorite <br /> Fashion Routines</h2>
+    return (
+        <div className='my-16 mx-8'>
+            <div className='flex justify-between items-end'>
+                <h2 className='text-6xl font-bold text-[#2e4053]'>Your Favorite <br /> Fashion Routines</h2>
+
+                <div>
+                    <Link href='/' className='flex bg-[#2e4053] py-4 px-5 sm:max-w-36 items-center justify-center rounded-full text-white font-bold hover:scale-95 duration-300'>Shop Now <MdArrowOutward className='text-xl ml-1' /> </Link>
+                </div>
+            </div>
 
             <div>
-                <Link href='/' className='flex bg-[#2e4053] py-4 px-5 sm:max-w-36 items-center justify-center rounded-full text-white font-bold hover:scale-95 duration-300'>Shop Now <MdArrowOutward className='text-xl ml-1' /> </Link>
-            </div>
-        </div>
+                <div className='grid grid-cols-3 gap-20 mt-5'>
+                    <div className='h-[700px]'>
+                        <video className='absolute h-[700px] -z-10' src='/test1.mp4' autoPlay loop muted />
+                        <div className='flex flex-col justify-between space-y-[30rem]'>
+                            <div className='py-10 mx-10 flex justify-start items-center gap-4 text-white'>
+                                <div className='border-2 border-inherit bg-white rounded-full w-12 h-12'>
+                                    <Image className='rounded-full' src='/bci-profile1.png' alt='profile picture of Miss Bee' width={50} height={50} />
+                                </div>
+                                <p className='font-bold text-3xl'>Bee March</p>
+                            </div>
 
-        <div className='grid grid-cols-3'>
-            <div>
-                <video src='/public/test1.mp4' autoPlay='true' loop='true' muted='true'/>
-                <div className=''>
-                    <Image src='' alt='' width={300} height={700} />
+                            <div className='flex justify-start bg-[whitesmoke] w-fit items-center'>
+                                <Image className='-mt-20' src="/Apple-Watch-Series-8.png" alt='Wristwatch' width={150} height={150}/>
+                                <p className='text-2xl w-min font-bold py-7 mr-24'>Wristwatch with class</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='h-[700px]'>
+                        <video className='absolute h-[700px] -z-10' src='/test1.mp4' autoPlay loop muted />
+                        <div className='flex flex-col justify-between space-y-[30rem]'>
+                            <div className='py-10 mx-10 flex justify-start items-center gap-4 text-white'>
+                                <div className='border-2 border-inherit bg-white rounded-full w-12 h-12'>
+                                    <Image className='rounded-full' src='/bci-profile1.png' alt='profile picture of Miss Bee' width={50} height={50} />
+                                </div>
+                                <p className='font-bold text-3xl'>Bee March</p>
+                            </div>
+
+                            <div className='flex justify-start bg-[whitesmoke] w-fit items-center'>
+                                <Image className='-mt-20' src="/Apple-Watch-Series-8.png" alt='Wristwatch' width={150} height={150}/>
+                                <p className='text-2xl w-min font-bold py-7 mr-24'>Wristwatch with class</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='h-[700px] '>
+                        <video className='absolute h-[700px] -z-10' src='/test1.mp4' autoPlay loop muted />
+                        <div className='flex flex-col justify-between space-y-[30rem]'>
+                            <div className='py-10 mx-10 flex justify-start items-center gap-4 text-white'>
+                                <div className='border-2 border-inherit bg-white rounded-full w-12 h-12'>
+                                    <Image className='rounded-full' src='/bci-profile1.png' alt='profile picture of Miss Bee' width={50} height={50} />
+                                </div>
+                                <p className='font-bold text-3xl'>Bee March</p>
+                            </div>
+
+                            <div className='flex justify-start bg-[whitesmoke] w-fit items-center'>
+                                <Image className='-mt-20' src="/Apple-Watch-Series-8.png" alt='Wristwatch' width={150} height={150}/>
+                                <p className='text-2xl w-min font-bold py-7 mr-24'>Wristwatch with class</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione fugiat, voluptatibus hic eos rerum, pariatur distinctio nobis aliquid suscipit voluptatem laboriosam ullam voluptate accusamus, unde quia accusantium vel! Quo, voluptates!
-                </div>
+
             </div>
-            <div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione fugiat, voluptatibus hic eos rerum, pariatur distinctio nobis aliquid suscipit voluptatem laboriosam ullam voluptate accusamus, unde quia accusantium vel! Quo, voluptates!
-                </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione fugiat, voluptatibus hic eos rerum, pariatur distinctio nobis aliquid suscipit voluptatem laboriosam ullam voluptate accusamus, unde quia accusantium vel! Quo, voluptates!
-                </div>
-            </div>
-            <div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione fugiat, voluptatibus hic eos rerum, pariatur distinctio nobis aliquid suscipit voluptatem laboriosam ullam voluptate accusamus, unde quia accusantium vel! Quo, voluptates!
-                </div>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione fugiat, voluptatibus hic eos rerum, pariatur distinctio nobis aliquid suscipit voluptatem laboriosam ullam voluptate accusamus, unde quia accusantium vel! Quo, voluptates!
-                </div>
-            </div>
-            
         </div>
-    </div>
-  )
+    )
 }
