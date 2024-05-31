801c70776743a8729eb2b3e4500a5b17798d60ad
BlarQ <collinsogunlala@gmail.com>
2024-05-31 13:37:28 +0100

    fix: responsiveness about page

--- src/app/components/AboutHead.jsx
+++ src/app/components/AboutHead.jsx
@@ -4,21 +4,22 @@ import React from 'react'
 export default function AboutHead() {
   return (
     <div className='mx-8 sm:space-y-10'>
-        <div className='lg:grid flex flex-col justify-center items-center lg:grid-flow-row grid-flow-col lg:grid-cols-3 sm:grid-rows-1 grid-rows-2 lg:items-end space-y-4 mt-10'>
-          <h2 className='sm:col-span-2 text-7xl font-bold sm:items-end items-center'>Chic Style, <br /> Happy Life</h2>
+        <div className='lg:grid flex flex-col justify-center items-center lg:grid-flow-row grid-flow-col lg:grid-cols-3 sm:grid-rows-1 grid-rows-2 lg:items-end space-y-4 md:mt-10 mt-5 pb-5 md:pb-0'>
+          <h2 className='sm:col-span-2 md:text-7xl text-4xl font-bold sm:items-end items-center'>Chic Style, <br /> Happy Life</h2>
 
-          <p className='flex justify-center sm:mx-4 items-center font-bold text-[gray] text-justify'>Step into a world where fashion meets joy. At our store, we believe that when you look good, you feel good.</p>
+          <p className='flex justify-center sm:mx-4 items-center text-sm md:text-base text-center font-bold text-[gray] md:text-justify'>Step into a world where fashion meets joy. At our store, we believe that when you look good, you feel good.</p>
         </div>
-        <div className='flex space-x-10'>
+        <div className='flex md:space-x-10 space-x-2'>
             <div className='flex flex-col justify-between space-y-5'>
-                <Image src='/cotton-bros.jpg' alt='Three guys on cotton shirts' width={800} height={700} />
-                <p className='mr-28 font-bold text-[gray]'>Discover an exquisite collection of clothes, wristwatches, and shoes that will elevate your style and brighten your days. Embrace the confidence that comes with chic outfits and stylish accessories. Shop with us and experience the happiness that a great wardrobe can bring.</p>
+                <Image className='md:w-full w-[400vw]' src='/cotton-bros.jpg' alt='Three guys on cotton shirts' width={800} height={700} />
+                <p className='hidden md:block md:mr-28 font-bold text-[gray]'>Discover an exquisite collection of clothes, wristwatches, and shoes that will elevate your style and brighten your days. Embrace the confidence that comes with chic outfits and stylish accessories. Shop with us and experience the happiness that a great wardrobe can bring.</p>
             </div>
 
             <div>
-                <Image src='/standing-man.jpg' alt='Standing man in suit' width={1200} height={1200} />
+                <Image className='md:w-full w-[100%]' src='/standing-man.jpg' alt='Standing man in suit' width={1200} height={1200} />
             </div>
         </div>
+        <p className='md:hidden mt-4 text-sm font-bold text-[gray]'>Discover an exquisite collection of clothes, wristwatches, and shoes that will elevate your style and brighten your days. Embrace the confidence that comes with chic outfits and stylish accessories. Shop with us and experience the happiness that a great wardrobe can bring.</p>
     </div>
   )
 }
--- src/app/components/DIscoverMission.jsx
+++ src/app/components/DIscoverMission.jsx
@@ -7,8 +7,8 @@ export default function DiscoverMission() {
   return (
     <div className='flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0 md:space-x-16 mx-4 md:mx-8 my-16 md:my-32 bg-gray-200 p-10 md:p-20'>
       <div>
-        <h2 className='text-4xl md:text-6xl font-bold max-w-full md:max-w-96'>Discover our mission</h2>
-        <p className='font-bold text-gray-600 my-4 md:w-fit'>
+        <h2 className='text-2xl md:text-6xl font-bold max-w-full md:max-w-96'>Discover our mission</h2>
+        <p className='font-bold text-sm md:text-base text-gray-600 my-4 md:w-fit'>
           At TrendLuxe, our mission is to empower individuals through fashion, offering a curated selection of high-quality clothing, wristwatches, and shoes that inspire confidence and joy. We are committed to exceptional craftsmanship, sustainable practices, and innovative designs that reflect the unique style of our customers. Our goal is to create a shopping experience that not only enhances your wardrobe but also elevates your everyday life.
         </p>
         <Link href='/' className='flex bg-[#2e4053] mt-6 md:mt-10 py-3 md:py-4 px-4 md:px-5 max-w-36 items-center justify-center rounded-full text-white font-bold hover:scale-95 duration-300'>
