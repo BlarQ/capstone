9ceb5a92c55dd4bbd2a1da09bbfa173b7b8bb181
BlarQ <collinsogunlala@gmail.com>
2024-06-11 14:46:31 +0100

    fix: localStorage

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,33 +1,33 @@
-"use client"
-import React, { createContext, useEffect, useState } from 'react'
+"use client";
+import React, { createContext, useEffect, useState } from 'react';
 
-const SideBarContext = createContext()
+const SideBarContext = createContext();
 
-const SidebarProvider = ({children}) => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [val, setVal] = useState(() => {
-      const savedVal = localStorage.getItem("Item num")
-      return savedVal !== null ? parseInt(savedVal, 10) : 0
-    })
-    const [inputVal, setInputVal] = useState(1);
-    const [cartItem, setCartItem] = useState(() => {
-      const savedCart = localStorage.getItem("cartItem")
-      return savedCart !== null? JSON.parse(savedCart) : []
-    })
+const SidebarProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [val, setVal] = useState(() => {
+    const savedVal = localStorage.getItem("Item num");
+    return savedVal !== null ? parseInt(savedVal, 10) : 0;
+  });
+  const [inputVal, setInputVal] = useState(1);
+  const [cartItem, setCartItem] = useState(() => {
+    const savedCart = localStorage.getItem("cartItem");
+    return savedCart !== null ? JSON.parse(savedCart) : [];
+  });
 
-    useEffect(() => {
-      localStorage.setItem("Item num", val)
-    }, [val])
+  useEffect(() => {
+    localStorage.setItem("Item num", val);
+  }, [val]);
 
-    useEffect(() => {
-      localStorage.setItem("cartItem", JSON.stringify(cartItem))
-    }, [cartItem])
+  useEffect(() => {
+    localStorage.setItem("cartItem", JSON.stringify(cartItem));
+  }, [cartItem]);
 
   return (
-    <SideBarContext.Provider value={{cartItem, setCartItem, isOpen, setIsOpen, val, setVal, inputVal, setInputVal}}>
-        {children}
+    <SideBarContext.Provider value={{ cartItem, setCartItem, isOpen, setIsOpen, val, setVal, inputVal, setInputVal }}>
+      {children}
     </SideBarContext.Provider>
-  )
-}
+  );
+};
 
-export {SidebarProvider, SideBarContext}
+export { SidebarProvider, SideBarContext };
