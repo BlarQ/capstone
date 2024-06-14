c9a488325c47029e52df0be4c7247e34e0537a13
BlarQ <collinsogunlala@gmail.com>
2024-06-14 02:27:01 +0100

    fix: mobile view of slug. pend: related prod

--- src/app/product/[slug]/page.jsx
+++ src/app/product/[slug]/page.jsx
@@ -27,11 +27,11 @@ export default function ProductPage({ params }) {
 
     return (
         <div className='mb-10'>
-            <div className='flex items-center justify-center px-8 py-10'>
-                <div className='w-[60%] bg-slate-200 rounded-xl flex justify-center items-center py-10'>
+            <div className='flex flex-col sm:flex-row items-center justify-center px-4 sm:px-8 sm:py-10'>
+                <div className='sm:w-[60%] bg-slate-200 rounded-xl flex justify-center items-center py-10'>
                     <Image src={`/${product.name.split(' ').join('-')}.png`} alt={product.name} width={400} height={400}/>
                 </div>
-                <div className='w-[40%] p-10'>
+                <div className='sm:w-[40%] sm:p-10'>
                     <h1 className='text-5xl font-bold my-5 text-[#2e4053]'>{product.name}</h1>
                     <p className='text-[gray] font-bold'>{product.description}</p>
                     <p className='font-bold text-3xl my-5 text-[gray]'><span>&#8358;</span>{product.amount.toLocaleString()}</p>
@@ -45,14 +45,14 @@ export default function ProductPage({ params }) {
 
             {/* Display related products */}
             {relatedProducts.length > 0 && (
-                <div className="mt-10 mx-8">
-                    <h2 className="text-3xl font-bold mb-5 text-[#2e4053]">Related Products</h2>
+                <div className="mt-10 sm:mx-8 mx-4">
+                    <h2 className="text-xl sm:text-3xl font-bold mb-5 text-[#2e4053]">Related Products</h2>
                     <div className="grid grid-cols-5 grid-rows-1 gap-5">
                         {relatedProducts.map((relatedProduct) => (
                             <div key={relatedProduct.id} className="flex justify-center bg-gray-200 rounded-xl p-2 hover:shadow-md duration-300">
                                 <Link href={`/product/${relatedProduct.name.split(' ').join('-')}`}>
 
-                                    <h3 className="text-lg font-bold mb-2 text-[#2e4053]">{relatedProduct.name}</h3>
+                                    <h3 className="sm:text-lg font-bold mb-2 text-[#2e4053]">{relatedProduct.name}</h3>
                                     <p className="font-bold mt-2 text-[#2e4053]">&#8358;{relatedProduct.amount.toLocaleString()}</p>
                                     <Image src={`/${relatedProduct.name.split(' ').join('-')}.png`} alt={relatedProduct.name} width={200} height={200}/>
                                 </Link>
