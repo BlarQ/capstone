7752ea45bf3a1884bed8f0a1a88e8e198cf9c8c4
BlarQ <collinsogunlala@gmail.com>
2024-06-15 15:35:33 +0100

    fix: footer adjusted

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -26,7 +26,7 @@ export default function Footer() {
 
               <section>
                 <section>
-                  <div className='flex flex-col md:gap-4 gap-2 items-center sm:items-start'>
+                  <div className='flex flex-col md:gap-4 gap-2 sm:items-start'>
                     <p className='text-xl font-bold'>Pages</p>
                     <Link className='hover:translate-x-1 duration-300 w-fit hover:text-[#e7e7e7]' href='/'>Home</Link>
                     <Link className='hover:translate-x-1 duration-300 w-fit hover:text-[#e7e7e7]' href='/about'>About</Link>
@@ -39,7 +39,7 @@ export default function Footer() {
 
               <section>
                 <section>
-                  <div className='flex flex-col md:gap-4 gap-2 items-center sm:items-start'>
+                  <div className='flex flex-col md:gap-4 gap-2 sm:items-start'>
                     <p className='text-xl font-bold'>Category</p>
                     <Link className='hover:translate-x-1 duration-300 w-fit hover:text-[#e7e7e7]' href='/shoecategory'>Shoes</Link>
                     <Link className='hover:translate-x-1 duration-300 w-fit hover:text-[#e7e7e7]' href='/shirtcategory'>Shirts</Link>
@@ -53,7 +53,7 @@ export default function Footer() {
 
               <section className='grid-flow-row'>
                 <section>
-                  <div className='flex flex-col md:gap-4 gap-2 items-center sm:items-start'>
+                  <div className='flex flex-col md:gap-4 gap-2 sm:items-start'>
                     <p className='sm:mb-6'></p>
                     <Link className='hover:translate-x-1 duration-300 w-fit hover:text-[#e7e7e7]' href='/'>FAQs</Link>
                     <Link className='hover:translate-x-1 duration-300 w-fit hover:text-[#e7e7e7]' href='/'>About Us</Link>
