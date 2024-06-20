2fa94676e58f950e4c1d418fa4d7f511a46ab08d
BlarQ <collinsogunlala@gmail.com>
2024-06-20 01:01:59 +0100

    feat: about 90% done

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,14 +1,43 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { FiArrowUpRight } from 'react-icons/fi'
 import { HiMail } from "react-icons/hi";
 import { IoArrowForwardCircleOutline } from 'react-icons/io5';
 import { MdEmojiEmotions } from 'react-icons/md';
 import { PiChatTextFill, PiHandbagSimpleFill, PiTagFill } from "react-icons/pi";
+import { GoPlus } from "react-icons/go";
 
-export default function page() {
+export default function Page() {
+  const [openFaq, setOpenFaq] = useState(false)
+  const [openFaq1, setOpenFaq1] = useState(false)
+  const [openFaq2, setOpenFaq2] = useState(false)
+  const [openFaq3, setOpenFaq3] = useState(false)
+  const [transform, setTransform] = useState(false)
+  const [transform1, setTransform1] = useState(false)
+  const [transform2, setTransform2] = useState(false)
+  const [transform3, setTransform3] = useState(false)
+
+  //Open and Close FAQs
+  function showFaq(){
+    setOpenFaq(!openFaq)
+    setTransform(!transform)
+  }
+  function showFaq1(){
+    setOpenFaq1(!openFaq1)
+    setTransform1(!transform1)
+  }
+  function showFaq2(){
+    setOpenFaq2(!openFaq2)
+    setTransform2(!transform2)
+  }
+  function showFaq3(){
+    setOpenFaq3(!openFaq3)
+    setTransform3(!transform3)
+  }
+
   return (
-    <div>
+    <div id='cont'>
         <section className='flex w-full justify-center items-start px-10 py-10 space-x-10'>
             <div className='w-1/2'>
                 <h1 className='text-7xl font-extrabold w-[90%]'>Get in touch with us today!</h1>
@@ -102,7 +131,7 @@ export default function page() {
               </div>
             </Link>
 
-            <Link href="mailto:collinsogunlala@gmail.com" className='bg-green-100 w-2/3 p-10 hover:scale-95 duration-300'>
+            <Link href="whatsapp://send?phone=2348174726892&text=Hello!" className='bg-green-100 w-2/3 p-10 hover:scale-95 duration-300'>
               <div className='pb-20'>
                 <div className='p-5 w-fit bg-white rounded-full'>
                   <PiChatTextFill className='text-5xl text-green-400'/>
@@ -124,30 +153,74 @@ export default function page() {
           </div>
         </section>
 
-        <section className='px-10 w-full flex items-start my-10'>
-          <div className='w-[40%]'>
+        <section className='px-10 w-full flex items-start my-10 gap-x-5'>
+          <div className='w-[40%] sticky top-36'>
             <div className='w-[90%]'>
               <h2 className='text-6xl font-bold'>Frequently asked questions</h2>
               <p className='py-5 font-semibold text-[gray] w-4/5'>Welcome to TrendLuxe&#39;s FAQ section! Here, you&#39;ll find answers to the most common questions about our products, services, and policies.</p>
 
-              <Link className='flex items-center justify-start py-2 px-4 bg-[#34c759] text-white font-semibold rounded-full gap-x-1 w-fit' href='#contact'>Contact
+              <Link className='flex items-center justify-start py-2 px-4 bg-[#34c759] text-white font-semibold rounded-full gap-x-1 w-fit' href='#cont'>Contact
                 <FiArrowUpRight />
               </Link>
             </div>
           </div>
 
-          <div className='text-4xl font-bold divide-y-2 border-y-2'>
-            <div className='py-10 px-2'>
-              <p>How do I place an order?</p>
+          <div className='text-4xl font-bold divide-y-2 border-y-2 w-[60%]'>
+            <div className='hover:pl-1 duration-300'>
+              <div className='py-10 px-2 flex justify-between items-center cursor-pointer' onClick={showFaq}>
+                <p>How do I place an order?</p>
+                <div className='text-3xl'>
+                  <GoPlus className={`${transform ? 'rounded-full rotate-45 text-white border-green-400 bg-green-400 font-bold duration-300' : 'duration-300 bg-[whitesmoke] shadow-sm border-2 rounded-full' }`}/>
+                </div>
+              </div>
+              {openFaq && 
+                <div>
+                  <p className='text-base font-semibold px-2 pb-10 selection:ease-in-out duration-700'>Placing an order with TrendLuxe is easy! Simply browse our online store, select your desired items, choose your size and color, and click &quot;Add to Cart.&quot; Once you&#39;re ready, proceed to checkout and follow the instructions to complete your purchase.</p>
+                </div>
+              }
+              
             </div>
-            <div className='py-10 px-2'>
-              <p>What payment methods do you accept?</p>
+            <div className='hover:pl-1 duration-300'>
+              <div className='py-10 px-2 flex justify-between items-center cursor-pointer group' onClick={showFaq1}>
+                <p>What payment methods do you accept?</p>
+                <div className='text-3xl'>
+                  <GoPlus className={`${transform1 ? 'rounded-full rotate-45 text-white border-green-400 bg-green-400 font-bold duration-300' : 'duration-300 bg-[whitesmoke] shadow-sm border-2 rounded-full' }`}/>
+                </div>
+              </div>
+              {openFaq1 && 
+                <div>
+                  <p className='text-base font-semibold px-2 pb-10 selection:ease-in-out duration-700'>We accept a variety of payment methods including credit cards (Visa, MasterCard, American Express), PayPal, and Apple Pay.</p>
+                </div>
+              }
+              
             </div>
-            <div className='py-10 px-2'>
-              <p> How can I track my order?</p>
+            <div className='hover:pl-1 duration-300'>
+              <div className='py-10 px-2 flex justify-between items-center cursor-pointer group' onClick={showFaq2}>
+                <p>How can I track my order?</p>
+                <div className='text-3xl'>
+                  <GoPlus className={`${transform2 ? 'rounded-full rotate-45 text-white border-green-400 bg-green-400 font-bold duration-300' : 'duration-300 bg-[whitesmoke] shadow-sm border-2 rounded-full' }`}/>
+                </div>
+              </div>
+              {openFaq2 && 
+                <div>
+                  <p className='text-base font-semibold px-2 pb-10 selection:ease-in-out duration-700'>After placing your order, you will receive a confirmation email with a tracking number. You can use this number to track your order on our website under the &quot;Track Order&quot; section.</p>
+                </div>
+              }
+              
             </div>
-            <div className='py-10 px-2'>
-              <p>How do I place an order?</p>
+            <div className='hover:pl-1 duration-300'>
+              <div className='py-10 px-2 flex justify-between items-center cursor-pointer group' onClick={showFaq3}>
+                <p>What is your return policy?</p>
+                <div className='text-3xl'>
+                  <GoPlus className={`${transform3 ? 'rounded-full rotate-45 text-white border-green-400 bg-green-400 font-bold duration-300' : 'duration-300 bg-[whitesmoke] shadow-sm border-2 rounded-full' }`}/>
+                </div>
+              </div>
+              {openFaq3 && 
+                <div>
+                  <p className='text-base font-semibold px-2 pb-10 selection:ease-in-out duration-700'>We offer a 30-day return policy on all our products. If you&apos;re not satisfied with your purchase, you can return it within 30 days of receipt for a full refund or exchange. Please ensure the items are in their original condition with all tags attached.</p>
+                </div>
+              }
+              
             </div>
           </div>
         </section>
