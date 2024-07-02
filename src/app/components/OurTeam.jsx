2338dcec48a9bede7f4fa505ca6de521ec68cee5
BlarQ <collinsogunlala@gmail.com>
2024-07-02 11:21:10 +0100

    add: team page

--- src/app/components/OurTeam.jsx
+++ src/app/components/OurTeam.jsx
@@ -45,19 +45,19 @@ export default function OurTeam() {
                         <div className='md:ml-2'>
                             <p className='text-xl mt-2 font-bold text-[gray]'>FrontEnd Developer</p>
                             <h2 className='text-4xl md:my-4 my-1 font-bold'>Adedamola O.</h2>
-                            <p className='font-bold text-[gray]'>Leads with a vision for innovation and excellence, guiding our brand to new heights.</p>
+                            <p className='font-bold text-[gray] text-justify'>As the frontend developer for this project, I have utilized my expertise in React.js, Next.js, and Tailwind CSS to create a seamless and user-friendly interface. My dedication to crafting responsive and visually appealing designs, combined with my strong problem-solving skills, has ensured that our web application provides an exceptional user experience.</p>
                         </div>
                     </div>
                     <div className='lg:flex lg:flex-col hover:text-[#34c759] cursor-pointer duration-500 sm:grid sm:grid-cols-2 sm:items-center gap-4'>
                         <div>
 
-                            <Image className='h-[40vh] sm:h-80 lg:h-[28rem]' src='/d2.jpg' alt='CEO of Trenluxe' width={400} height={400} />
+                            <Image className='h-[40vh] sm:h-80 lg:h-[28rem]' src='/mrfranc.jpg' alt='CEO of Trenluxe' width={400} height={400} />
                         </div>
 
                         <div className='md:ml-2'>
                             <p className='text-xl mt-2 font-bold text-[gray]'>Project Supervisor</p>
-                            <h2 className='text-4xl md:my-4 my-1 font-bold'>Franc Emepuo</h2>
-                            <p className='font-bold text-[gray]'>He excels in creating intuitive, user-friendly designs that enhance user experience. His collaborative approach and technical expertise make him an invaluable asset to our team.</p>
+                            <h2 className='text-4xl md:my-4 my-1 font-bold'>Mr. Franc Enemuo</h2>
+                            <p className='font-bold text-[gray] text-justify'>His insightful feedback, vast knowledge, and encouraging mentorship have been instrumental in driving the success of our work. His leadership has not only enhanced my skills but has also inspired me to strive for excellence in every aspect of the project.</p>
                         </div>
                     </div>
                     <div className='lg:flex lg:flex-col hover:text-[#34c759] cursor-pointer duration-500 sm:grid sm:grid-cols-2 sm:items-center gap-4'>
